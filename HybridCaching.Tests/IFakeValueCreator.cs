9a82cf6636068ad09e0d8e5a52305e712f715d3b
Felipe Castro <felipe.mayor@vtex.com.br>
2015-06-09 13:18:29 -0300

    Tests HybridCache

---
+++ HybridCaching.Tests/IFakeValueCreator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HybridCaching.Tests
+{
+    public interface IFakeValueCreator
+    {
+        Task<string> CreateString();
+        Task<Dictionary<string, string>> CreateDictionary();
+    }
+}
---
+++ HybridCaching.Tests/ResourceFactory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HybridCaching.Backends.InProcess;
+using HybridCaching.Backends.Redis;
+using HybridCaching.Interfaces;
+using StackExchange.Redis;
+
+namespace HybridCaching.Tests
+{
+    public static class ResourceFactory
+    {
+        public static IRedisAdapter GetRedisAdapter()
+        {
+            var multiplexer = ConnectionMultiplexer.Connect("localhost:6379");
+
+            return new RedisAdapter(multiplexer);
+        }
+
+        public static RedisCache GetRedisCache()
+        {
+            return new RedisCache(GetRedisAdapter());
+        }
+
+        public static InProcessCache GetInProcessCache()
+        {
+            return new InProcessCache("IntegrationTests");
+        }
+
+        public static Stack<IRawCache> GetCacheStack()
+        {
+            return new Stack<IRawCache>(new List<IRawCache>{GetRedisCache(), GetInProcessCache()});
+        }
+
+        public static Stack<IRawCache> GetCacheStack(params IRawCache[] cacheBackends)
+        {
+            return new Stack<IRawCache>(cacheBackends);
+        }
+
+        public static HybridCache GetHybridCache()
+        {
+            return new HybridCache(GetCacheStack());
+        }
+
+        public static HybridCache GetHybridCache(params IRawCache[] cacheBackends)
+        {
+            return new HybridCache(GetCacheStack(cacheBackends));
+        }
+
+        private static string GenerateString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random((int)DateTime.Now.Ticks);
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GenerateKey()
+        {
+            return String.Format("testKey:{0}", GenerateString(8));
+        }
+
+        public static Dictionary<string, string> GenerateDictionary(int size)
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            for (var i = 0; i < size; i++)
+            {
+                dictionary.Add(GenerateKey() + i, GenerateString(10));
+            }
+
+            return dictionary;
+        }
+    }
+}
