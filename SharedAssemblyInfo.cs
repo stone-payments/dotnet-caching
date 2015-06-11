8b6a6aff7444a7a2ca6d7fc052c4d8bbfccf037c
Rodrigo Dumont <rlsdumont@gmail.com>
2015-06-11 14:45:24 -0300

    Release v0.1.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
  *  --pre [value]   Use `value` as the prerelease identifier
  *  --stable        Bump to a stable version instead of prerelease
  */
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
