351d2a10c4add336a578af2485d83799ca4fde71
Felipe Castro <felipecastrosm@gmail.com>
2016-04-29 15:58:42 -0300

    Creates CacheKeyEvent type

---
+++ src/Caching/CacheKeyEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Vtex.Caching.Enums;
+
+namespace Vtex.Caching
+{
+    public class CacheKeyEvent
+    {
+        public string CacheKey { get; set; }
+
+        public EventType EventType { get; set; }
+
+        public string CacheBackendIdentifier { get; set; }
+
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
